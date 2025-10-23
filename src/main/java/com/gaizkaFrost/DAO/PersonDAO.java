6ff782bca917c850d81f5f4886dd36c4f9005f0d
GaizkaDM <gaizkaDm@gmail.com>
2025-10-23 09:45:53 +0200

    FIX corregida logica de sql para que este todo igual

--- src/main/java/com/gaizkaFrost/DAO/PersonDAO.java
+++ src/main/java/com/gaizkaFrost/DAO/PersonDAO.java
@@ -38,9 +38,9 @@ public class PersonDAO {
             while (rs.next()) {
                 persons.add(new Person(
                         rs.getInt("id"),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getDate("birth_date").toLocalDate()
+                        rs.getString("nombre"),
+                        rs.getString("apellido"),
+                        rs.getDate("fecha_nacimiento").toLocalDate()
                 ));
             }
         } catch (SQLException e) {
